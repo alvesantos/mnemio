c26b4fcf4e187b77d92a289aaceaa99ad087a8b6
alvesantos <alvesantos.gh@gmail.com>
2026-09-05 19:39:02 -0300

    Add environment configuration for API URL

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000';
 const TOKEN_KEY = 'mnemio_token';
 
 export const api = axios.create({ baseURL: API_URL });
